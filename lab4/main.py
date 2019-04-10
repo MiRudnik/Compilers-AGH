7206dd3701016ea2786c3bab5d9af44856d9b6a9
maknapik <m.knapik6@gmail.com>
2019-04-10 13:07:15 +0200

    init lab4

---
+++ lab4/main.py
@@ -0,0 +1,24 @@
+import sys
+import ply.yacc as yacc
+import lab2.Mparser as Mparser
+import lab3.TreePrinter as TreePrinter
+from lab4.TypeChecker import TypeChecker
+
+if __name__ == '__main__':
+
+    try:
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        file = open(filename, "r")
+    except IOError:
+        print("Cannot open {0} file".format(filename))
+        sys.exit(0)
+
+    Mparser = Mparser()
+    parser = yacc.yacc(module=Mparser)
+    text = file.read()
+
+    ast = parser.parse(text, lexer=Mparser.scanner)
+
+    # Below code shows how to use visitor
+    typeChecker = TypeChecker()
+    typeChecker.visit(ast)  # or alternatively ast.accept(typeChecker)
